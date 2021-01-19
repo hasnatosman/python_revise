59c642b9cc9237b24757e7d7996bc3c025061bc1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-01-19 12:03:13 +0600

    Add files via upload
    checking even or odd number.

---
+++ calculation.py
@@ -0,0 +1,13 @@
+def test():
+    if x % 2 == 0:
+        print("It is an even number.!")
+
+    elif x % 2 != 0:
+        print("It is an odd number.!")
+
+    else:
+        print('It is not a number.')
+
+
+x = 55
+test()
