2aa8187abe73ed38b2895f67ef5a28b20257711d
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-01-21 15:18:16 +0600

    Add files via upload

---
+++ pyDict.py
@@ -0,0 +1,19 @@
+name = {
+    1: 'Hasnat',
+    2: 'Osman',
+    3: 'ashik'
+}
+
+id = [12, 22, 33]
+player = ['Has', 'nat', 'osm']
+values = dict(zip(id, player))
+
+
+print(name)
+print(name.get(2))
+
+print('\n')
+print('\n')
+print('\n')
+
+print(values)
