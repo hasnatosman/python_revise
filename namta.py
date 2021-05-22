25104994cde3e91d3be61670e06ca31bac7e8d21
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-05-22 21:51:06 +0600

    Add files via upload

---
+++ namta.py
@@ -0,0 +1,12 @@
+print('Please enter a number!!!! ')
+number = int(input('Number is: '))
+
+count = 1
+
+while count <= 10:
+    print(number, 'x', count, '=', number*count)
+    count= count + 1
+
+
+print()
+print('So cool !!!!!!!!!!')
