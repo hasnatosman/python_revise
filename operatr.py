a66b4736148143511338f5bcb4f9edef326a627c
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-12 21:06:01 +0600

    Add files via upload

---
+++ operatr.py
@@ -0,0 +1,28 @@
+"""
+There are a number of operators in python,
+some of them discussed here.
+"""
+
+#arithmetic
+
+x = 9
+y = 4
+
+z = x + y
+print('Addition of x and y: ', z)
+
+z = x - y
+print('Subtraction of x and y :', z)
+
+z = x / y
+print(' Division of x by y :', z)
+
+
+z = x // y
+print('Floor division: ', z)
+
+z = x % y
+print('Modulus or reminder :', z)
+
+z = x ** y
+print('Exponent of x and y : ', z)
